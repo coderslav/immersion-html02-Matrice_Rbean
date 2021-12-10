53b87d5f00e04f3093dbe705beb35272490b1430
rkomin <kominslav@gmail.com>
2021-12-10 16:13:08 +0100

    fixes

--- changeColor/changeColor.js
+++ changeColor/changeColor.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     let styleSheet = document.createElement('style');
-    styleSheet.innerHTML = 'div { display: inline-block; padding: 1rem 1rem 2rem; } div#square {display: block; width: 500px; height: 500px; background-color: blue; content: "";}' + '#blue:hover ~ #square{background-color: blue;}' + '#red:hover ~ #square{background-color: red;}' + '#yellow:hover ~ #square{background-color: yellow;}' + '#green:hover ~ #square{background-color: green;}' + '#black:hover ~ #square{background-color: black;}';
+    styleSheet.innerHTML = 'div { display: inline-block; padding: 1rem 1rem 2rem; } div#square {display: block; width: 500px; height: 500px; background-color: blue; content: "";}';
     document.head.appendChild(styleSheet);
+
+    let divList = document.getElementsByTagName('div');
+
+    for (let index = 0; index < divList.length; index++) {
+        if (divList[index].textContent !== '') {
+            divList[index].addEventListener('mouseover', () => {
+                document.getElementById('square').style.backgroundColor = divList[index].textContent.toLowerCase();
+            });
+            divList[index].addEventListener('mouseout', () => {
+                document.getElementById('square').style.backgroundColor = 'blue';
+            });
+        }
+    }
 });
