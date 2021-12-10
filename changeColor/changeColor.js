3bb40894f29a850606dd82fd6a368b6c128faa7b
rkomin <kominslav@gmail.com>
2021-12-10 15:16:08 +0100

    fix for SEEENTINEL

--- changeColor/changeColor.js
+++ changeColor/changeColor.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementsByTagName('style')[0].innerHTML = 'div { display: inline-block; padding: 1rem 1rem 2rem; } div#square {display: block; width: 500px; height: 500px; background-color: blue; content: "";}' + '#blue:hover ~ #square{background-color: blue;}' + '#red:hover ~ #square{background-color: red;}' + '#yellow:hover ~ #square{background-color: yellow;}' + '#green:hover ~ #square{background-color: green;}' + '#black:hover ~ #square{background-color: black;}';
+    let styleSheet = document.createElement('style');
+    styleSheet.innerHTML = 'div { display: inline-block; padding: 1rem 1rem 2rem; } div#square {display: block; width: 500px; height: 500px; background-color: blue; content: "";}' + '#blue:hover ~ #square{background-color: blue;}' + '#red:hover ~ #square{background-color: red;}' + '#yellow:hover ~ #square{background-color: yellow;}' + '#green:hover ~ #square{background-color: green;}' + '#black:hover ~ #square{background-color: black;}';
+    document.head.appendChild(styleSheet);
 });
